f67f338184b6b2a27d3a1eadf015beaa4cb48984
sanskarsharma <sanskar2996@gmail.com>
2020-08-30 20:01:33 +0530

    just go fmt

--- main.go
+++ main.go
@@ -1,27 +1,27 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
-	"net/http"
-	"io/ioutil"
-	"os"
-	"strings"
 	"image"
 	"image/color"
-    "encoding/json"
-	"bytes"
-	"log"
-	"os/exec"
 	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+	"strings"
 
 	"github.com/disintegration/imaging"
 	"github.com/gorilla/mux"
 )
 
 func checkErr(e error) {
-    if e != nil {
-        panic(e)
-    }
+	if e != nil {
+		panic(e)
+	}
 }
 
 func isVideo(contentType string) bool {
@@ -33,11 +33,11 @@ func isVideo(contentType string) bool {
 		checkErr(err)
 	}
 
-    for _, supportedContentType := range supportedContentTypes {
-        if contentType == supportedContentType {
+	for _, supportedContentType := range supportedContentTypes {
+		if contentType == supportedContentType {
 			return true
 		}
-    }
+	}
 	return false
 }
 
@@ -50,15 +50,15 @@ func isImage(contentType string) bool {
 		checkErr(err)
 	}
 
-    for _, supportedContentType := range supportedContentTypes {
-        if contentType == supportedContentType {
+	for _, supportedContentType := range supportedContentTypes {
+		if contentType == supportedContentType {
 			return true
 		}
-    }
+	}
 	return false
 }
 
-func generateImageThumbnail(file *os.File) *os.File  {
+func generateImageThumbnail(file *os.File) *os.File {
 	sourcImg, err := imaging.Open(file.Name())
 	checkErr(err)
 
@@ -71,7 +71,7 @@ func generateImageThumbnail(file *os.File) *os.File  {
 	thumbnailImg = imaging.Paste(thumbnailImg, thumbnail, image.Pt(0, 0))
 
 	// save the combined image to file
-	tempFile , err := ioutil.TempFile("", "thumbnail*.png")
+	tempFile, err := ioutil.TempFile("", "thumbnail*.png")
 	checkErr(err)
 	err = imaging.Save(thumbnailImg, tempFile.Name())
 	checkErr(err)
@@ -81,14 +81,14 @@ func generateImageThumbnail(file *os.File) *os.File  {
 
 func generateVideoThumbnail(url string) *os.File {
 
-	tempDir , err := ioutil.TempDir("", "thumbnail*")
+	tempDir, err := ioutil.TempDir("", "thumbnail*")
 	checkErr(err)
 
 	outputFilePath := tempDir + "/thumbnail.png"
 
 	cmd := `ffmpeg -i "%s" -an -q 0 -vf scale="'if(gt(iw,ih),-1,200):if(gt(iw,ih),200,-1)', crop=200:200:exact=1" -vframes 1 "%s"`
 	// ffmpeg cmd ref : https://gist.github.com/TimothyRHuertas/b22e1a252447ab97aa0f8de7c65f96b8
-	
+
 	cmdSubstituted := fmt.Sprintf(cmd, url, outputFilePath)
 
 	shellName := "ash" // for docker (using alpine image)
@@ -99,7 +99,7 @@ func generateVideoThumbnail(url string) *os.File {
 	ffCmd := exec.Command(shellName, "-c", cmdSubstituted)
 
 	// getting real error msg : https://stackoverflow.com/questions/18159704/how-to-debug-exit-status-1-error-when-running-exec-command-in-golang
-    output, err := ffCmd.CombinedOutput()
+	output, err := ffCmd.CombinedOutput()
 	if err != nil {
 		log.Println(fmt.Sprint(err) + ": " + string(output))
 		checkErr(err)
@@ -119,24 +119,24 @@ func uploadFile(file *os.File, uploadUrl string) {
 	uploadRequest, err := http.NewRequest("PUT", uploadUrl, bytes.NewBuffer(fileBytes))
 	checkErr(err)
 	httpClient := &http.Client{}
-    resp, err := httpClient.Do(uploadRequest)
+	resp, err := httpClient.Do(uploadRequest)
 	checkErr(err)
-	
+
 	fmt.Println(resp.Status)
 	fmt.Println(resp.Body)
 	respBody, err := ioutil.ReadAll(resp.Body)
 	checkErr(err)
-    defer resp.Body.Close()
+	defer resp.Body.Close()
 	fmt.Println(string(respBody))
 
 	if resp.StatusCode != http.StatusOK {
 		fmt.Println("Non-OK HTTP status:", resp.StatusCode)
-		panic("uploading thumbnail failed") 
+		panic("uploading thumbnail failed")
 	}
 
 }
 
-func handleThumbify(w http.ResponseWriter, r *http.Request)  {
+func handleThumbify(w http.ResponseWriter, r *http.Request) {
 
 	// Read body
 	body, err := ioutil.ReadAll(r.Body)
@@ -155,13 +155,13 @@ func handleThumbify(w http.ResponseWriter, r *http.Request)  {
 	}
 
 	// check if both requried fields are present
-	if (thumbnailRequestPayload.DownloadUrl == "" || thumbnailRequestPayload.UploadUrl == "") {
+	if thumbnailRequestPayload.DownloadUrl == "" || thumbnailRequestPayload.UploadUrl == "" {
 		w.WriteHeader(400)
 		json.NewEncoder(w).Encode(map[string]string{"message": "download_url or upload_url key not present in request data"})
 		return
 	}
 
-	// real work starts from here 
+	// real work starts from here
 
 	// make GET request with "Range" header to get partial content in response and check response header for content type
 	// this ^ is valid for s3 URLs only and is a workaround since HEAD requests cannot be made to s3 presigned URLs (ref : https://stackoverflow.com/a/39663152/7314323)
@@ -171,7 +171,7 @@ func handleThumbify(w http.ResponseWriter, r *http.Request)  {
 	checkErr(err)
 	defer downloadResponse.Body.Close()
 	if !(downloadResponse.StatusCode >= 200 && downloadResponse.StatusCode <= 299) {
-		// raise 422 
+		// raise 422
 		w.WriteHeader(422)
 		jsonResponse, _ := json.Marshal(map[string]string{
 			"message": fmt.Sprintf("download url returned %d status code", downloadResponse.StatusCode),
@@ -184,14 +184,13 @@ func handleThumbify(w http.ResponseWriter, r *http.Request)  {
 	log.Println("response headers are: ", downloadResponse.Header.Get("content-type"))
 
 	contentType := downloadResponse.Header.Get("content-type")
-	
 
-	var outputFile *os.File 
+	var outputFile *os.File
 	if isImage(contentType) {
 		contentTypeSplit := strings.Split(contentType, "/")
-		extension := contentTypeSplit[len(contentTypeSplit) - 1]
+		extension := contentTypeSplit[len(contentTypeSplit)-1]
 
-		tempFile , err := ioutil.TempFile("", "download*." + extension) // '*' will be populated with a random numeric string
+		tempFile, err := ioutil.TempFile("", "download*."+extension) // '*' will be populated with a random numeric string
 		checkErr(err)
 		defer os.Remove(tempFile.Name())
 		log.Println("temp file name:", tempFile.Name())
@@ -205,7 +204,7 @@ func handleThumbify(w http.ResponseWriter, r *http.Request)  {
 	} else if isVideo(contentType) {
 		outputFile = generateVideoThumbnail(thumbnailRequestPayload.DownloadUrl)
 	} else {
-		// raise 422 
+		// raise 422
 		w.WriteHeader(422)
 		jsonResponse, _ := json.Marshal(map[string]string{
 			"message": "Un-supported content type",
@@ -214,19 +213,17 @@ func handleThumbify(w http.ResponseWriter, r *http.Request)  {
 		return
 	}
 	log.Println("thumbnail file :", outputFile.Name())
-	
-	// upload file 
+
+	// upload file
 	// todo : raise error if upload response non 200
 	uploadFile(outputFile, thumbnailRequestPayload.UploadUrl)
 
 }
 
-
 type ThumbnailRequestPayload struct {
 	DownloadUrl string `json:"download_url"`
-	UploadUrl string `json:"upload_url"`
+	UploadUrl   string `json:"upload_url"`
 }
-
 
 func recoveryMiddleware(handler http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -248,20 +245,19 @@ func recoveryMiddleware(handler http.Handler) http.Handler {
 }
 
 func contentTypeMiddleware(handler http.Handler) http.Handler {
-    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        w.Header().Add("Content-Type", "application/json")
-        handler.ServeHTTP(w, r)
-    })
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Content-Type", "application/json")
+		handler.ServeHTTP(w, r)
+	})
 }
 
-func main()  {
+func main() {
 	router := mux.NewRouter()
 	router.Use(contentTypeMiddleware)
 	router.Use(recoveryMiddleware)
 
-    router.HandleFunc("/thumbify", handleThumbify).Methods("POST")
+	router.HandleFunc("/thumbify", handleThumbify).Methods("POST")
 
 	log.Println("Listening...")
 	http.ListenAndServe(":2712", router)
 }
-
